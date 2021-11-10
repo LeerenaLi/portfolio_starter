400fa103494b0114bbdf6f63e2261a3b4cfde22f
Leerena <leerena7@gmail.com>
2021-11-10 14:32:24 +0300

    added hamburger and menu, script for menu

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+// menu
+
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
