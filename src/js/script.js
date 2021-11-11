1672c13ce8dbe5a2aaa8ef180971e9233e5c30db
Leerena <leerena7@gmail.com>
2021-11-11 11:12:40 +0300

    added grid part skills_progress and percent display script

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,12 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+//skills__progress
+
+const counters = document.querySelectorAll('.skills__progress-counter'),
+      lines = document.querySelectorAll('.skills__progress-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
